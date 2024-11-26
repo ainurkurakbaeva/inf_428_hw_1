a3462434e900a2b844aba4802cbe23bd354a8655
Ainur <kurakbaeva.ab@gmail.com>
2024-11-27 00:18:43 +0500

    2 - O(m + n)

--- task_1/leetcodeSolutions.py
+++ task_1/leetcodeSolutions.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 class Solution:
     def findLengthOfLCIS(self, nums: List[int]) -> int:
         maxLen = count = 1
@@ -13,4 +12,26 @@ class Solution:
             maxLen = max(count, maxLen)
 
         return maxLen
+
+#     task2
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i, j, k = m - 1, n - 1, m + n - 1
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+
+#             task3
+
+
 
